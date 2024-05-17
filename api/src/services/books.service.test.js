037ea274bb665ee24e675baa8751cbb3590be3f5
Wiliams Ixcoy <ixcoywiliams@gmail.com>
2024-05-17 17:28:06 -0600

    uso de datos falsos para pruebas

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,34 +1,38 @@
 const BookService = require('./books.service');
+const { generateManyBooks } = require('../fix/book.fake');
 
+const mockGetAll = jest.fn();
 
-const fakeBooks = [
-  {
-    _id: 1,
-    name: "Harry Potter",
-  },
-];
+// va a suplantar MongoLib
 
+/*
 
-//va a suplantar MongoLib
-const MongoLibStub={
-getAll:()=>[...fakeBooks],
-create:()=>{}
-}
-//que genere una suplantación de esta libreria
-jest.mock("../lib/mongo.lib",()=> jest.fn().mockImplementation(()=>MongoLibStub));
+const MongoLibStub = {
+  // decimos que get all va a ser igual al espia, para ver el comportamiento.
+  getAll: mockGetAll,
+  create: () => {},
+};
+*/
 
+// que genere una suplantación de esta libreria
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
+  getAll: mockGetAll,
+  create: () => {},
+})));
 
-//primera prueba
+// primera prueba
 describe('Test for BookService', () => {
   let service;
   beforeEach(() => {
     service = new BookService();
-    //para limpiar todos los mocks y pasar a nuevos datos en caso sea necesario
+    // para limpiar todos los mocks y pasar a nuevos datos en caso sea necesario
     jest.clearAllMocks();
   });
 
-  describe('test for getBooks',()=>{
-    //primera prueba
+  describe('test for getBooks', () => {
+    /*
+
+      //primera prueba
     test('should return a list book', async () => {
       //arrange
       //act
@@ -47,8 +51,24 @@ describe('Test for BookService', () => {
       //assert
       expect(books[0].name).toEqual("Harry Potter");
     })
-  })
+
+    */
+
+    // tercera prueba
+    test('should return a list book 3, items', async () => {
+      const fakeBooks = generateManyBooks(20);
+      // arrange
+
+      // agregamos al espia los valores que va a usar para resolver
+      mockGetAll.mockResolvedValue([...fakeBooks]);
+      // act
+      const books = await service.getBooks({});
+      console.log(books);
+      // assert
+      expect(books.length).toEqual(fakeBooks.length);
+      expect(mockGetAll).toHaveBeenCalled();
+      expect(mockGetAll).toHaveBeenCalledWith('books', {});
+      expect(mockGetAll).toHaveBeenCalledTimes(1);
+    });
+  });
 });
-
-
-
