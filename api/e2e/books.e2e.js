11e200b4e43302310d14a2a962981fef786935e3
Wiliams Ixcoy <ixcoywiliams@gmail.com>
2024-05-24 17:40:19 -0600

    pruebas e2e con base de datos de prueba

--- api/e2e/books.e2e.js
+++ api/e2e/books.e2e.js
@@ -1,41 +1,59 @@
-const mockGetAll = jest.fn();
 const request = require('supertest');
-
+const { MongoClient } =require("mongodb")
+const {config} = require("../src/config/index")
 const createApp = require('../src/app');
 
-
-const { generateManyBooks } = require('../src/fix/book.fake');
-
-
-
-
-jest.mock('../src/lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
-    getAll: mockGetAll,
-    create: () => {},
-  })));
-
+const DB_NAME = config.dbName;
+const MONGO_URI = config.dbUrl
 
 describe('Test for books endpoints', () => {
-  let app = null;
+    let app = null;
     let server = null;
-    beforeAll( () => {
-    app = createApp();
-    server = app.listen(3004)
+    let database = null;
+
+    beforeAll( async () => {
+        app = createApp();
+        server = app.listen(3004)
+
+        //conexion a la bd de pruebas 
+        const client = new MongoClient(MONGO_URI,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+        });
+        await client.connect();
+        database = client.db(DB_NAME)
     });
 
-    afterAll(async()=>{
+    afterAll( async () => {
         await server.close();
+        await database.close();
+        //una vez termine de usarse la bd se borra
+        await database.dropDatabase()
     });
 
     describe('test for [GET] /api/v1/books', () => {
-        test('should return a list books', () => {
-            const fakeBooks = generateManyBooks(3);
-            
-            mockGetAll.mockResolvedValue([...fakeBooks]);
 
-            //el slash al principio de api es importante para las pruebas asi debe quedar /endpoint
+        test('should return a list books',async () => {            
+            //arrange
+            
+            //semilla de informacion
+            const seedData = await database.collection('books').insertMany([
+                {
+                    name:'libro 1',
+                    year:1998,
+                    author:'Wiliams'
+                },
+                {
+                    name:'libro 2',
+                    year:1998,
+                    author:'Wiliams'
+                }
+            ])
+
+            //act
             return request(app).get('/api/v1/books').expect(200).then(({body}) => {
-                expect(body.length).toEqual(fakeBooks.length);
+                //assert
+                expect(body.length).toEqual(seedData.insertedCount);
             })
         });
     });
